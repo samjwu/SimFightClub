fd2c2b36dd6f9178ea178af625f1970096748c06
Sam Wu <sjwu@ualberta.ca>
2022-07-16 16:39:45 -0600

    add scriptable object for enemy data

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public string enemyName;
+    public Texture enemyPicture;
+}
