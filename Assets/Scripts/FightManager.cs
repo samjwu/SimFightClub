969cb241005a6acb09cba770c90da5d65d318199
Sam Wu <sjwu@ualberta.ca>
2022-07-16 15:08:34 -0600

    add fight manager

---
+++ Assets/Scripts/FightManager.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FightManager : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI _enemyName;
+    [SerializeField]
+    Image _enemyImage;
+    [SerializeField]
+    TextMeshProUGUI _explanation;
+}
--- Assets/Scripts/PlayerStatistics.cs
+++ Assets/Scripts/PlayerStatistics.cs
@@ -3,7 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Statistics : MonoBehaviour
+public class PlayerStatistics : MonoBehaviour
 {
     public enum StatType
     {
