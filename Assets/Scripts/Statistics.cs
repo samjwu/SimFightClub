3286f6521b1fde21e3db314acc90ba614f0fb7c4
Sam Wu <sjwu@ualberta.ca>
2022-07-15 19:38:57 -0600

    add statistics text update method

--- Assets/Scripts/Statistics.cs
+++ Assets/Scripts/Statistics.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -29,6 +30,17 @@ public class Statistics : MonoBehaviour
     [SerializeField]
     Button _minusIntButton;
 
+    [SerializeField]
+    TextMeshProUGUI _strText;
+    [SerializeField]
+    TextMeshProUGUI _agiText;
+    [SerializeField]
+    TextMeshProUGUI _intText;
+    [SerializeField]
+    TextMeshProUGUI _pointText;
+    [SerializeField]
+    TextMeshProUGUI _statText;
+
     void Start()
     {
         _plusStrButton.onClick.AddListener(delegate { ChangeStat(StatType.Strength, 1); });
@@ -37,11 +49,6 @@ public class Statistics : MonoBehaviour
         _minusAgiButton.onClick.AddListener(delegate { ChangeStat(StatType.Agility, -1); });
         _plusIntButton.onClick.AddListener(delegate { ChangeStat(StatType.Intelligence, 1); });
         _minusIntButton.onClick.AddListener(delegate { ChangeStat(StatType.Intelligence, -1); });
-    }
-
-    void Update()
-    {
-
     }
 
     void ChangeStat(StatType type, int value)
@@ -62,5 +69,34 @@ public class Statistics : MonoBehaviour
         }
 
         statPoints -= value;
+        UpdateStatisticsText(type);
+    }
+
+    void UpdateStatisticsText(StatType type)
+    {
+        switch (type)
+        {
+            case StatType.Strength:
+                _strText.text = string.Format("Strength: {0}", strength);
+                break;
+            case StatType.Agility:
+                _agiText.text = string.Format("Agility: {0}", agility);
+                break;
+            case StatType.Intelligence:
+                _intText.text = string.Format("Intelligence: {0}", intelligence);
+                break;
+            default:
+                throw new Exception("Invalid stat type");
+        }
+
+        _pointText.text = string.Format("Stat Points: {0}", statPoints);
+        _statText.text = string.Format(
+            "Hit Points: {}\n" +
+            "Damage: {}\n" +
+            "Dodge Chance: {}\n" +
+            "Attack Speed: {}\n" +
+            "Defense: {}\n" +
+            "Critical Strike Damage: {}"
+            );
     }
 }
