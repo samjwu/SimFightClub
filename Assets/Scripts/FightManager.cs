71b5cbed24f704e02a247f30412b678b263d3f34
Sam Wu <sjwu@ualberta.ca>
2022-07-16 20:14:30 -0600

    perform fight damage calculations

--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -16,15 +16,49 @@ public class FightManager : MonoBehaviour
 
     EnemyData _currentEnemy;
 
+    double _currentPlayerHp;
+    double _currentEnemyHp;
+
     void Start()
     {
         _currentEnemy = enemyList[Random.Range(0, enemyList.Count)];
         _enemyName.text = _currentEnemy.enemyName;
         _enemyImage.texture = _currentEnemy.enemyPicture;
+
+        _currentPlayerHp = PlayerStatistics.hp;
+        _currentEnemyHp = _currentEnemy.hp;
     }
 
     void Update()
     {
-        
+        double playerHitChance = 100 - _currentEnemy.dodgeChance;
+        double enemyHitChance = 100 - PlayerStatistics.dodgeChance;
+
+        bool isPlayerAccurate = playerHitChance >= Random.Range(0, 101);
+        bool isEnemyAccurate = enemyHitChance >= Random.Range(0, 101);
+
+        bool isPlayerHitCritical = 0.1 >= Random.Range(0, 101);
+        bool isEnemyHitCritical = 0.1 >= Random.Range(0, 101);
+
+        double playerDamageDealt = PlayerStatistics.damage * (isPlayerHitCritical ? PlayerStatistics.criticalStrikeDamage : 1) - _currentEnemy.defense;
+        double enemyDamageDealt = _currentEnemy.damage * (isEnemyHitCritical ? _currentEnemy.criticalStrikeDamage / 100 : 1) - PlayerStatistics.defense;
+
+        if (isPlayerAccurate)
+        {
+            _currentEnemyHp -= playerDamageDealt;
+        }
+        else
+        {
+
+        }
+
+        if (isEnemyAccurate)
+        {
+            _currentPlayerHp -= enemyDamageDealt;
+        }
+        else
+        {
+
+        }
     }
 }
